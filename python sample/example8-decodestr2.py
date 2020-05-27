e83c4af4fecb3601950449c66673878d9b9279d6
charles2gan <gjden@qq.com>
2020-05-27 20:08:39 +0800

    Update example8-decodestr2.py

--- python sample/example8-decodestr2.py
+++ python sample/example8-decodestr2.py
@@ -1,6 +1,6 @@
 import GdaImport
 #gjden
-#example of dumping the code of method or class
+#example of decoding strings which is located by bytecode fingerprint.
 def printStringHex(stri): 
     ret=''
     for ch in stri:
@@ -27,7 +27,7 @@ def decodeString(gda,idx):
 def GDA_MAIN(gda_obj):
     gda=gda_obj
     Dex0=gda.DexList[0]
-    midx=0x4fc9
+    midx=0x4fc9 #index of decoding method in dex
     method=Dex0.MethodTable[str(midx)]
     clist=method.callorIdxList
     destr=''
@@ -40,25 +40,30 @@ def GDA_MAIN(gda_obj):
             continue
         callorTable[sidx]=idx
         callor=Dex0.MethodTable[sidx]
+        #dump bytecode of the callors
         callorHex=gda.DumpHexData(callor.offset+0x10,callor.size-0x10,callor.size-0x10,0)
-        callorHex=callorHex[0:-2]
+        #callorHex=callorHex[0:-2]
         #gda.log(callorHex)
         start=8
         end=len(callorHex)      
         while True:
+            #c94f is the string of index 0x4fc9
             pos=callorHex.find('c94f',start,end-1)
             if pos<0:
                 break;
             start=pos+1
             if callorHex[pos-4:pos-2]=='71':
+                #find the index of the encoded string.
                 strIdx1=callorHex[pos-6:pos-4]
                 strIdx2=callorHex[pos-8:pos-6]
                 strIdx=strIdx1+strIdx2
+                #check if this string is decoded
                 if strIdxTable.has_key(strIdx):
                     continue    
                 strIdxTable[strIdx]=strIdx
+                #decode the string
                 dstr=decodeString(gda,int(strIdx,16))
-                #gda.SetStringById(int(strIdx,16),dstr)
+                #output 
                 destr+="[string@"
                 destr+=strIdx
                 destr+="] "
